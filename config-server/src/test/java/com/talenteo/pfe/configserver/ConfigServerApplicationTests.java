633b141014d87969e451ceb13e2681004c783862
youssef <youssef.jemaiel@nearteam.fr>
2024-12-17 23:52:34 +0100

    first commit

---
+++ config-server/src/test/java/com/talenteo/pfe/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talenteo.pfe.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
